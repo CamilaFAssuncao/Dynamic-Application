f77f5fdb03d96276ecdeb30f6b76802dc0ee33cd
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-06-29 12:13:02 +0200

    adding 5 days forecast

--- script.js
+++ script.js
@@ -1,92 +1,110 @@
 const apiKey = "4afb1285974cc14b3c84791c156150d4";
 
-
-// Get the necessary DOM elements
-let cityInput = document.getElementById("cityInput");
+const cityInput = document.getElementById("cityInput");
 const submitButton = document.querySelector(".submit");
-let output = document.getElementById("output");
+const output = document.getElementById("output");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector(".tempCelsius span");
 const descElement = document.querySelector(".description");
 const weatherIconElement = document.querySelector("#weatherIcon");
 const maxTempElement = document.querySelector("#max-temp span");
 const minTempElement = document.querySelector("#min-temp span");
+const followingDays = document.getElementById("followingDays")
+const notFound = document.getElementById("not-found")
+const searchBar = document.querySelector(".searchBar")
 
-// Event listener for the submit button
-submitButton.addEventListener("click", predictTemp);
+submitButton.addEventListener("click", handleButtonClick);
+cityInput.addEventListener("keyup", handleEnterKey);
 
-// Event listener for Enter key press
-cityInput.addEventListener("keydown", pressEnter);
+function handleButtonClick(e) {
+  e.preventDefault();
+  const city = cityInput.value;
+  showWeatherData(city);
+}
 
-function pressEnter(event) {
-  // Check if the Enter key was pressed
-  if (event.key === "Enter") {
-    let city = cityInput.value;
-    predictTemp(city);
+function handleEnterKey(e) {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+    showWeatherData(city);
   }
 }
 
-function predictTemp(city) {
-    // Retrieve the value of the city input
-    let cityName = cityInput.value;
-    getWeatherData(cityName)
-      .then(data => {
-        // Update the DOM elements with the fetched data
-        cityElement.innerText = data.name;
-        tempElement.innerText = parseInt(data.main.temp);
-        descElement.innerText = data.weather[0].description;
-        weatherIconElement.setAttribute(
-          "src",
-          `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-        );
-        maxTempElement.innerText = parseInt(data.main.temp_max);
-        minTempElement.innerText = parseInt(data.main.temp_min);
-        
-        // Show the weather data by removing the "hide" class from the output element
-        output.classList.remove("hide");
-      })
-      .catch(error => {
-        console.log("Error: ", error);
-      });
+async function showWeatherData(city) {
+    try {
+      const [currentData, forecastData] = await Promise.all([
+        getCurrentWeatherData(city),
+        getForecastData(city),
+      ]);
+  
+      // Display current weather
+      cityElement.innerText = currentData.name;
+      tempElement.innerText = Math.round(currentData.main.temp);
+      descElement.innerText = currentData.weather[0].description;
+      weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${currentData.weather[0].icon}.png`
+      );
+      maxTempElement.innerText = Math.round(currentData.main.temp_max);
+      minTempElement.innerText = Math.round(currentData.main.temp_min);
+      output.classList.remove("hide");
+      followingDays.classList.remove("hide");
+  
+      // Display forecast for the next 5 days
+      for (let i = 0; i < 5; i++) {
+        const minTempElement = document.getElementById("day" + (i + 1) + "Min");
+        const maxTempElement = document.getElementById("day" + (i + 1) + "Max");
+        const iconElement = document.getElementById("img" + (i + 1));
+  
+        const minTemp = Math.round(forecastData.list[i].main.temp_min);
+        const maxTemp = Math.round(forecastData.list[i].main.temp_max);
+        const icon = forecastData.list[i].weather[0].icon;
+  
+        minTempElement.innerHTML = minTemp + "°C";
+  maxTempElement.innerHTML = maxTemp + "°C";
+        iconElement.src = "http://openweathermap.org/img/wn/" + icon + ".png";
+      }
+  
+    } catch (error) {
+      notFound.classList.remove("hide");
+      console.log("Error: ", error);
+    }
   }
   
-const getWeatherData = async (city) => {
-  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=en`;
-  const res = await fetch(apiWeatherURL);
-  const data = await res.json();
-  return data;
-};
+  async function getCurrentWeatherData(city) {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=en`;
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+    return data;
+  }
+  
+  async function getForecastData(city) {
+    const apiForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=en`;
+    const res = await fetch(apiForecastURL);
+    const data = await res.json();
+    return data;
+  }
 
-const showWeatherData = async (city) => {
-  // Call the getWeatherData function to fetch weather data
-  const data = await getWeatherData(city);
-
-  // Update the DOM elements with the fetched data
-  cityElement.innerText = data.name;
-  tempElement.innerText = parseInt(data.main.temp);
-  descElement.innerText = data.weather[0].description;
-  weatherIconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-  );
-
-  // Show the weather data by removing the "hide" class from the output element
-  output.classList.remove("hide");
-};
-
-// // Event listener for the submit button click
-// submitButton.addEventListener("click", async (e) => {
-//   e.preventDefault();
-
-//   const city = cityInput.value;
-//   showWeatherData(city);
-// });
-
-// // Event listener for Enter key press on the city input field
-// cityInput.addEventListener("keyup", (e) => {
-//   if (e.code === "Enter") {
-//     const city = e.target.value;
-//     showWeatherData(city);
-//   }
-// });
-
+  for (let i = 0; i < 5; i++) {
+    const dayElement = document.getElementById("day" + (i + 1));
+    const iconElement = document.getElementById("img" + (i + 1));
+    const dayNameElement = document.getElementById("day" + (i + 1) + "Name");
+    const tempElement = document.getElementById("day" + (i + 1) + "Temp");
+  
+    const dayData = forecastData.list[i];
+    const dayName = getDayName(dayData.dt);
+    const icon = dayData.weather[0].icon;
+    const temp = Math.round(dayData.main.temp);
+  
+    dayElement.innerHTML = dayName;
+    iconElement.src = "http://openweathermap.org/img/wn/" + icon + ".png";
+    dayNameElement.innerHTML = dayName;
+    tempElement.innerHTML = temp + "°C";
+  }
+  
+  function getDayName(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const dayIndex = date.getDay();
+    return days[dayIndex];
+  }
+  
